2b2372f8eb002a81fd74ba3142fbfbc344f5737f
Diogo Barroso <ei11105@fe.up.pt>
2017-08-24 10:14:32 +0100

    Version 0.1 done

--- Assets/Scripts/DummyScript.cs
+++ Assets/Scripts/DummyScript.cs
@@ -4,22 +4,36 @@ using System.Collections;
 public class DummyScript : MonoBehaviour {
 
     Animator anim;
+    public int speed;
+    Vector3 target;
 	// Use this for initialization
 	void Start () {
+        target = new Vector3(8000, -2000, transform.position.z);
         anim = GetComponent<Animator>();
         anim.enabled = false;
+        speed = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target, step);
+
+        //Cheat for animation control
         if (Input.GetKeyDown(KeyCode.Space))
         {
             anim.enabled = !anim.enabled;
+            speed = 300;
         }
 	}
 
     public void setAnimation(bool state)
     {
         anim.enabled = state;
+    }
+
+    public void setSpeed(int newSpeed)
+    {
+        speed = newSpeed;
     }
 }
--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -1,12 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class Manager : MonoBehaviour {
-	// Use this for initialization
+    // Use this for initialization
+    GameObject panel;
+    GameObject Dummy;
 	void Start () {
-        DontDestroyOnLoad(transform.gameObject);
-	}
+        //DontDestroyOnLoad(transform.gameObject);
+        panel = Instantiate(Resources.Load("Panel") as GameObject);
+        panel.transform.SetParent(GameObject.Find("Canvas").transform);
+        Vector3 pos = new Vector3(0, 0, 0);
+        panel.transform.localPosition = pos;
+        panel.SetActive(false);
+        Dummy = GameObject.Find("Dummy");
+    }
 	
     public void checkOperation()
     {
@@ -14,16 +24,43 @@ public class Manager : MonoBehaviour {
         int part1 = System.Int32.Parse(GameObject.Find("Part1").GetComponentInChildren<Text>().text);
         int part2 = System.Int32.Parse(GameObject.Find("Part2").GetComponentInChildren<Text>().text);
         int rest = System.Int32.Parse(GameObject.Find("Rest").GetComponentInChildren<Text>().text);
-        int amountObjects = GameObject.FindGameObjectsWithTag("Objects").Length;
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("Objects");
+        int amountObjects = objs.Length;
         Debug.Log(part1 + " + " + part2 + " = " + whole);
 
         if (part1 + part2 == whole && part1 + part2 + whole + rest == amountObjects)
         {
-            GameObject.Find("Dummy").GetComponent<Animator>().enabled = true;
+            StartCoroutine(Success(objs));
         }
     }
 	// Update is called once per frame
 	void Update () {
         	
 	}
+
+    IEnumerator Success(GameObject[] objs)
+    {
+        GameObject.Find("Rest").SetActive(false);
+        foreach (GameObject obj in objs)
+            obj.SetActive(false);
+
+        for (int i = 0; i < 3; i++)
+            Dummy.transform.GetChild(i).GetComponentInChildren<Text>().enabled = false;
+        GameObject.Find("Dummy").GetComponent<Animator>().enabled = true;
+        GameObject.Find("Dummy").GetComponent<DummyScript>().setSpeed(300);
+
+        yield return new WaitForSeconds(2);
+        panel.SetActive(true);
+        for (int i = 0; i < 2; i++)
+        {
+            GameObject.Find("StarBG" + i).GetComponent<Image>().color = Color.white;
+        }
+        panel.GetComponentInChildren<Button>().onClick.AddListener(() => nextLevel());
+    }
+
+    public void nextLevel()
+    {
+        int index = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(index+1);
+    }
 }
